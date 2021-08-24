14db17da861dc0cb7a817f22bf42da0dcb09dd4f
sarori <salo3678@gmail.com>
2021-08-24 23:55:03 +0200

    Dropdown menu

--- src/routes/Header/Header.js
+++ src/routes/Header/Header.js
@@ -1,5 +1,15 @@
 import { useHistory } from "react-router-dom"
-import { HeaderContent, HeaderCover, HeaderWrapper, HStyle } from "./HeaderStyles"
+import {
+	DropdownBtn,
+	DropdownContent,
+	DropdownLst,
+	HeaderContent,
+	HeaderCover,
+	HeaderWrapper,
+	HStyle,
+	Lst,
+	UList,
+} from "./HeaderStyles"
 
 const Header = () => {
 	const history = useHistory()
@@ -10,45 +20,21 @@ const Header = () => {
 		<HeaderWrapper>
 			<HeaderCover></HeaderCover>
 			<HeaderContent>
-				<HStyle.UList>
-					<HStyle.Lst>
+				<UList>
+					<Lst>
 						<div onClick={() => history?.push(`/`)}>Home</div>
-					</HStyle.Lst>
-					<HStyle.Lst className="dropdown">
-						<div className="dropbtn" onClick={() => history?.push(`/profile/intro`)}>
-							Profile
-						</div>
-						<div className="dropdown-content">
+					</Lst>
+					<DropdownLst className="dropdown">
+						<DropdownBtn>Profile</DropdownBtn>
+						<DropdownContent>
 							<div onClick={() => history.push(`/profile/intro`)}>Intro</div>
 							<div onClick={() => history.push(`/profile/lan`)}>Lan & Skills</div>
-						</div>
-					</HStyle.Lst>
-					<HStyle.Lst>
+						</DropdownContent>
+					</DropdownLst>
+					<Lst>
 						<div onClick={() => history?.push(`/contact`)}>Contact</div>
-					</HStyle.Lst>
-				</HStyle.UList>
-				{/* <HStyle.UList>
-					<HStyle.List onClick={() => history?.push(`/`)}>Home</HStyle.List>
-					<HStyle.Dropdown>
-						Profile
-						<HStyle.DropdownContent class="dropdown-content">
-							<div onClick={() => history.push(`/profile/intro`)}>Intro</div>
-							<div onClick={() => history.push(`/profile/lan`)}>Lan & Skills</div>
-						</HStyle.DropdownContent>
-					</HStyle.Dropdown>
-					<HStyle.List onClick={() => history.push(`/projects`)}>Projects</HStyle.List>
-					<HStyle.Dropdown>
-						Experiences
-						<HStyle.DropdownContent class="dropdown-content">
-							<div onClick={() => history.push(`/experiences/career`)}>Career</div>
-							<div onClick={() => history.push(`/experiences/awards`)}>Awards</div>
-							<div onClick={() => history.push(`/experiences/activities`)}>
-								Activities
-							</div>
-						</HStyle.DropdownContent>
-					</HStyle.Dropdown>
-					<HStyle.List onClick={() => history.push(`/contact`)}>Contact</HStyle.List>
-				</HStyle.UList> */}
+					</Lst>
+				</UList>
 			</HeaderContent>
 		</HeaderWrapper>
 	)
